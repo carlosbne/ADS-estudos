214b1136ece7bcc4f9e415c4242dc33ddd689d03
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-07-19 19:21:02 -0300

    add new one q 2

---
+++ 2024.1/POO-java/exercicios/questao-2/Author.java
@@ -0,0 +1,13 @@
+package questao-2;
+
+public class Author {
+    //classe author
+
+class Author{
+    private String name;
+    private String email;
+    private List<Book> books;
+  
+    
+  }
+}
---
+++ 2024.1/POO-java/exercicios/questao-2/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+
+}
