af0042b06498c02e488a6b9e4374782815797b80
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-07-30 23:10:34 -0300

    malloc e free funcoes

--- 2024.1/Algoritmo-c/exercicios-struct/malloc.c
+++ 2024.1/Algoritmo-c/exercicios-struct/malloc.c
@@ -14,3 +14,26 @@ int main(void) {
   printf("\n");
   return 0;
 }
+/* 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  int *a;
+  int n;
+  scanf("%d", &n);
+  a = (int*)malloc(sizeof(int) * n);
+  printf("Endereço do início da memória: " "%p (%lld)\n", a, (long long)a); // Verifica local do indereço da memoria 
+  if(a == 0){
+    printf("ERRO ao alocar memória");
+    exit(1);
+  }
+  for(int i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  for(int i = 0; i < n; i++)
+    printf("%d ", a[i]);
+  printf("\n");
+  free(a);
+  return 0;
+} */
