661f8f08f9dbca9e36ed45be58f46be8aec57f9f
carlosbne <106997150+carlosbne@users.noreply.github.com>
2024-09-05 11:02:12 -0300

    insertioSort att

--- 2024.1/Algoritmo-c/ordenacao/java/InsertionSort/InsertionSort.java
+++ 2024.1/Algoritmo-c/ordenacao/java/InsertionSort/InsertionSort.java
@@ -1,5 +1,9 @@
-public class InsertionSort {
-    public void inserSort(int arr[]){
+import java.util.Random;
+
+ class Insert {
+    public void insertionSort(int arr[]){
+        long tempoInicial = System.currentTimeMillis(); 
+
         for(int i = 0; i < arr.length; i++){
             int verifica = i;                                         //define primeiro elemento a ser verificado como o incial
             while(verifica > 0 && arr[verifica] < arr[verifica - 1]){ //enquanto verif > 0 E o elemento na posiçao verificada < q posição verif-1
@@ -9,6 +13,30 @@ public class InsertionSort {
                 verifica -= 1;
             }
         }
+        //calcula o tempo 
+        long tempoFinal = System.currentTimeMillis();
+        System.out.println();
+        /* System.out.println(tempoFinal +"\n"); */
+        long tempoTotal =  tempoFinal - tempoInicial;
+        System.out.println("O método foi executado em " + (tempoTotal/* /1000 */) + " ms\n");
+
+        System.out.println("Array Ordenada: ");
+        imprimir(arr);                         //chamada para imprimir ordenado
+
+    }
+
+    //função para gerar conjunto de dados aleatórios
+    void gerador(int tamanho, int limite){
+        int[] arr = new int[tamanho];            //cria array definindo tamanho
+  
+        for(int i = 0; i < tamanho; i++){        //laço para preencher array com números gerados
+            Random rand = new Random();
+            int number = rand.nextInt(limite);
+            arr[i] = number;
+        } 
+        System.out.println("Array gerada: ");
+        imprimir(arr);                          //chamada imprimir dados aleatórios
+        insertionSort(arr);                        //chamada para ordenar
     }
 
     //funcao para imprimir o array
@@ -18,3 +46,10 @@ public class InsertionSort {
         }
     }
 }
+    public class InsertionSort{
+        public static void main(String args[]) {
+          Insert obj = new Insert();
+          obj.gerador(10, 100);        //chama função geradora passando o temanho e limite do arr
+        }
+    }
+
