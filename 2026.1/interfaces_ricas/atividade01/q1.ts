cfab950798199164639c6cc72026a90c503b15fc
carlosbne <106997150+carlosbne@users.noreply.github.com>
2026-04-03 14:57:09 -0300

    docs: organizado e atividades de interfaces ricas

---
+++ 2026.1/interfaces_ricas/atividade01/q1.ts
@@ -0,0 +1,21 @@
+/* 1. Faça um programa TypeScript que calcule o quadrado de cada elemento do array, use as seguintes estratégias:
+a. iterando com for simples
+b. iterando com forEach */
+
+//a.
+let arr =  [3,5,7,3,8,9,1];
+
+for(let i = 0; i < arr.length; i++){
+    arr[i] *= arr[i];
+}
+
+console.log(arr)
+
+
+//b.
+let arr1 =  [3,5,7,3,8,9,1];
+let arrQuadrado:Number[] = [];  //definindo tipo do array
+
+arr1.forEach((el) => arrQuadrado.push(el*el));
+
+console.log(arrQuadrado)
