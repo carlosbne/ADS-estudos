4639b39f031120b9d67d7604c04b33013eadf4a2
carlosbne <106997150+carlosbne@users.noreply.github.com>
2026-04-03 20:02:28 -0300

    docs: att pastas

---
+++ 2026.1/interfaces_ricas/atividade01/q2.ts
@@ -0,0 +1,8 @@
+/* 2. Faça um Programa TypeScript que transforme o array, concatenando as strings com 1 (um) espaço (“ “). Utilize o método JOIN da classe Array, passando uma função arrow como parâmetro.
+2.1 Escreva um teste com o array ['Arrays, 'com', 'TypeScript] */
+
+let testeString = ['Arrays', 'com', 'TypeScript'];
+
+const concatenaArray = () => testeString.join(" ");
+
+console.log(concatenaArray())
